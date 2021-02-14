dab550df6fce6401efaddde9ed31d334f72ea52e
Hannes Kuhl <hanneskuhl@live.de>
2021-02-14 15:35:49 +0100

    forward action started

--- main.go
+++ main.go
@@ -5,55 +5,73 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+// Flows are the main object in a configuration file
 type Flows struct {
 	Flows []Flow `json:"flows"`
 }
 
+// Flow describes one specific flow
 type Flow struct {
-	Id               int              `json:"id"`
-	Name             string           `json:"name"`
-	Responsibilities []Responsibility `json:"responsibilities"`
-	EventKeys        []WhatWhere      `json:"eventKeys"`
+	ID               int                  `json:"id"`
+	Name             string               `json:"name"`
+	Responsibilities []WhatWhereHowString `json:"responsibilities"`
+	EventKeys        []WhatWhereHowString `json:"eventKeys"`
+	Actions          []Action             `json:"actions"`
 }
 
-type Responsibility struct {
-	Where string `json:"where"`
-	What  string `json:"what"`
-	How   string `json:"how"`
+// WhatWhereHowString has all optional parameters giving specifics for a Flow-task
+type WhatWhereHowString struct {
+	Where string `json:"where,omitempty"`
+	What  string `json:"what,omitempty"`
+	How   string `json:"how,omitempty"`
 }
 
-type WhatWhere struct {
-	Where string `json:"where"`
-	What  string `json:"what"`
+// Action describes the specifics of an action
+type Action struct {
+	What       string     `json:"what"`
+	Where      string     `json:"where,omitempty"`
+	HowForward HowForward `json:"howForward,omitempty"`
+	Then       []*Action  `json:"then,omitempty"`
 }
 
+// HowForward describes what a forward action looks like
+type HowForward struct {
+	RequestMethod string               `json:"requestMethod"`
+	Headers       []WhatWhereHowString `json:"headers,omitempty"`
+	Body          []WhatWhereHowString `json:"body,omitempty"`
+	Query         []WhatWhereHowString `json:"query,omitempty"`
+}
+
+// Event describes all possible parameters that an event can have
 type Event struct {
 	Hostname string
 	Path     string
-	RemoteIp string
-	Headers  map[string]*Header
+	RemoteIP string
+	Headers  map[string]*ValueString
 }
 
-type Header struct {
+// ValueString only has a value
+type ValueString struct {
 	value string
 }
 
 func main() {
 	// Use an external setup function in order
 	// to configure the app in tests as well
-	app := Setup()
+	app := setup()
 
 	// start the application on http://localhost:3000
 	log.Fatal(app.Listen(":3000"))
 }
 
 func getInitConfig() Flows {
-	file, err := ioutil.ReadFile("assets/config-example.json")
+	file, err := ioutil.ReadFile("assets/config-example-simple.json")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -85,6 +103,7 @@ func isThisFlowResponsible(flow Flow, c *fiber.Ctx) bool {
 				} else if parts[2] == "RequestURI" {
 					where = c.Hostname() + c.Path()
 				}
+				// @TODO: add option for url-query parameters
 			case "Header":
 				where = c.Get(parts[2])
 			default:
@@ -140,7 +159,7 @@ func getResponsibleFlow(c *fiber.Ctx) (Flow, bool) {
 	return claimingFlow, requestHasBeenClaimed
 }
 
-func Setup() *fiber.App {
+func setup() *fiber.App {
 	// Initialize a new app
 	app := fiber.New()
 
@@ -149,10 +168,12 @@ func Setup() *fiber.App {
 		flow, claimed := getResponsibleFlow(c)
 
 		if claimed {
-			event := GetEventData(c, flow)
+			event := getEventData(c, flow)
 			fmt.Println("----------------------")
 			fmt.Println(event)
 			fmt.Println("----------------------")
+
+			runActions(flow.Actions, event)
 
 			return c.JSON(flow)
 		} else {
@@ -163,7 +184,7 @@ func Setup() *fiber.App {
 	return app
 }
 
-func GetEventData(c *fiber.Ctx, flow Flow) Event {
+func getEventData(c *fiber.Ctx, flow Flow) Event {
 	event := new(Event)
 
 	for _, eventKey := range flow.EventKeys {
@@ -179,7 +200,7 @@ func GetEventData(c *fiber.Ctx, flow Flow) Event {
 			case "Path":
 				event.Path = c.Path()
 			case "IP":
-				event.RemoteIp = c.IP()
+				event.RemoteIP = c.IP()
 			}
 		} else if whereMethod == "Get" {
 			// check if the current event already has
@@ -187,17 +208,59 @@ func GetEventData(c *fiber.Ctx, flow Flow) Event {
 			headers := event.Headers
 			if len(headers) == 0 {
 				// initialize a map of strings that point to
-				// structs of type Header
-				event.Headers = make(map[string]*Header)
+				// structs of type ValueString
+				event.Headers = make(map[string]*ValueString)
 			}
 
 			header, headerExists := event.Headers[whereKey]
 			if !headerExists {
-				header = &Header{}
+				header = &ValueString{}
 			}
 			event.Headers[whereKey] = header
 			event.Headers[whereKey].value = c.Get(whereKey)
 		}
 	}
 	return *event
+}
+
+func runActions(actions []Action, e Event) {
+	for _, action := range actions {
+		switch action.What {
+		case "forward":
+			client := &http.Client{}
+
+			requestURL := makeRequestURL(action, e)
+			req, reqErr := http.NewRequest(action.HowForward.RequestMethod, requestURL, nil)
+			if reqErr != nil {
+				log.Fatal(reqErr)
+				return
+			}
+
+			for _, headerToAdd := range action.HowForward.Headers {
+				req.Header.Add(headerToAdd.Where, headerToAdd.What)
+			}
+			resp, respErr := client.Do(req)
+			if respErr != nil {
+				log.Fatal(respErr)
+				return
+			}
+
+			fmt.Println("forward action", resp)
+		case "process":
+			fmt.Println("process action")
+		}
+	}
+}
+
+func makeRequestURL(action Action, e Event) string {
+	url := action.Where
+	for idx, query := range action.HowForward.Query {
+		if idx == 0 {
+			url += "?"
+		} else {
+			url += "&"
+		}
+		url += query.Where + "=" + query.What
+	}
+	return url
 }
